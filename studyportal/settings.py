557e1a6e040823b33141ec4f75e4329b05ccb1a4
Harshit Maurya <hmaurya999@gmail.com>
2019-04-16 04:47:32 +0530

    Add rest_framework to known apps

--- studyportal/settings.py
+++ studyportal/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_api'
+    'rest_api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
