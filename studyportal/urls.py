844197b6615d9faa710d74e27f1c73d2e5045a8e
Harshit Maurya <hmaurya999@gmail.com>
2019-04-16 04:48:03 +0530

    API versioning

--- studyportal/urls.py
+++ studyportal/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rest_api.urls'))
+    path('api/v1/', include('rest_api.urls'))
 ]
