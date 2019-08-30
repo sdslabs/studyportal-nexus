f7d4d3ff73d280f6764e2d5376c3a23762d72e1f
AyanChoudhary <darkrider251099@gmail.com>
2019-08-31 01:50:09 +0530

    added url field to department and made courseApi

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,7 +1,13 @@
 from rest_api.models import Department
+from rest_api.models import Course
 from rest_framework import serializers
 
 class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Department
         fields = ('title', 'abbreviation', 'url')
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('title','department')
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from rest_api.models import Department
+from rest_api.models import Course
 from rest_framework  import viewsets
 from rest_api.serializers import DepartmentSerializer
+from rest_api.serializers import CourseSerializer
 
 def sample(request):
     return HttpResponse("Test endpoint")
@@ -9,3 +11,13 @@ def sample(request):
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+
+    def get_queryset(self):
+        queryset = Course.objects.all()
+        department = self.request.query_params.get('department')
+        queryset = Course.objects.filter(department = department)
+
+        return queryset
