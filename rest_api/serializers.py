31884a7886d20fd8f4b2f2edf42041a62aef6808
AyanChoudhary <darkrider251099@gmail.com>
2019-10-08 04:04:46 +0530

    made fileUpload changes to file model and made dynamic renderring functions from file data

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,4 +1,4 @@
-from rest_api.models import Department, Course, File
+from rest_api.models import Department, Course, File, Request
 from rest_framework import serializers
 
 class DepartmentSerializer(serializers.ModelSerializer):
@@ -19,8 +19,37 @@ class CourseSerializer(serializers.ModelSerializer):
         def create(self, validated_data):
             return Course.objects.create(**validated_data)
 
+def fileName(file):
+    filename = file.split('/')[-1]
+    return filename.split('.')[-2]
+
 class FileSerializer(serializers.ModelSerializer):
     course = CourseSerializer()
+    size = serializers.SerializerMethodField()
+    title = serializers.SerializerMethodField()
+    fileext = serializers.SerializerMethodField()
     class Meta:
         model = File
-        fields = ('id', 'path', 'title', 'downloads', 'size', 'date_modified', 'filetype', 'course')
+        fields = ('id', 'path', 'title', 'downloads', 'size', 'date_modified', 'fileext', 'filetype', 'course')
+    def get_size(self, obj):
+        file_size = ''
+        if obj.file and hasattr(obj.file, 'size'):
+            file_size = obj.file.size
+        if round(file_size/(1024*1024),2) == 0.00:
+            return str(round(file_size/(1024),2))+" KB"
+        else:
+            return str(round(file_size/(1024*1024),2))+" MB"
+    def get_title(self, obj):
+        file_title = ''
+        if obj.file and hasattr(obj.file, 'name'):
+            file_title = obj.file.name
+        return fileName(file_title)
+    def get_fileext(self, obj):
+      filename = obj.file.name
+      return filename.split('.')[-1]
+
+class RequestSerializer(serializers.ModelSerializer):
+    course = CourseSerializer()
+    class Meta:
+        model = Request
+        fields = ('id', 'user_id', 'filetype', 'status', 'title', 'course')
