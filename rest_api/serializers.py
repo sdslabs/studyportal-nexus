bea9de7e6936eb4692fe17ba61955363d09a7650
AyanChoudhary <darkrider251099@gmail.com>
2019-10-31 22:21:04 +0530

    fixed post request for files

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -15,38 +15,15 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ('id', 'title', 'department', 'code')
 
-    # def create(self, validated_data):
-    #     department_data = validated_data.pop('department')
-    #     return Course.objects.create(department=department_data, **validated_data)
-
 def fileName(file):
     filename = file.split('/')[-1]
     return filename.split('.')[-2]
 
 class FileSerializer(serializers.ModelSerializer):
-    course = CourseSerializer()
-    size = serializers.SerializerMethodField()
-    title = serializers.SerializerMethodField()
-    fileext = serializers.SerializerMethodField()
+    course = CourseSerializer(Course.objects.all())
     class Meta:
         model = File
         fields = ('id', 'title', 'driveid', 'downloads', 'size', 'date_modified', 'fileext', 'filetype', 'course')
-    def get_size(self, obj):
-        file_size = ''
-        if obj.file and hasattr(obj.file, 'size'):
-            file_size = obj.file.size
-        if round(file_size/(1024*1024),2) == 0.00:
-            return str(round(file_size/(1024),2))+" KB"
-        else:
-            return str(round(file_size/(1024*1024),2))+" MB"
-    def get_title(self, obj):
-        file_title = ''
-        if obj.file and hasattr(obj.file, 'name'):
-            file_title = obj.file.name
-        return fileName(file_title)
-    def get_fileext(self, obj):
-      filename = obj.file.name
-      return filename.split('.')[-1]
 
 class RequestSerializer(serializers.ModelSerializer):
     course = CourseSerializer()
