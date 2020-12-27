efad89022c3c5eb8a980ba8c1b2afb7a17883c30
mhk19 <gmahak1@gmail.com>
2020-12-28 02:25:54 +0530

    notify users when admin uploads a file

--- rest_api/utils.py
+++ rest_api/utils.py
@@ -1,7 +1,8 @@
 from users.models import User
 from users.serializers import UserSerializer
-from rest_api.serializers import DepartmentSerializer, CourseSerializer
+from rest_api.serializers import DepartmentSerializer, CourseSerializer, FileSerializer
 from users.signals import notification_handler
+from rest_api.models import Course
 
 
 def add_course(course, department):
@@ -18,3 +19,20 @@ def add_course(course, department):
             notification_type="addcourse", target=department,
             link="/departments/" + department_code + "/courses/" + course_code
         )
+
+def add_file(file, course):
+    file.save()
+    file_data = FileSerializer(file).data
+    user_list = User.objects.all()
+    recipient_list = UserSerializer(user_list, many=True)
+    recipients = recipient_list.data[:]
+    for recipient in recipients:
+        for course_id in recipient['courses']:
+            if course == Course.objects.get(id=course_id):
+                serializer_course = CourseSerializer(course)
+                department = serializer_course.data['department']
+                department_code = DepartmentSerializer(department).data['abbreviation']
+                notification_handler(recipient=recipient['id'], actor="Admin",
+                                     verb="added a file", action=file_data['title'],
+                                     notification_type="addfile", target=course,
+                                     link="/departments/" + department_code + "/courses/" + file_data['course']['code'])
