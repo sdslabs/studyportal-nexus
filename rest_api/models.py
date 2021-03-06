11669ed190cb7381f6a5f868ea191db60b4461bf
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2021-03-07 00:25:24 +0530

    feat: add imageField in department to fetch department thumbnails from backend

--- rest_api/models.py
+++ rest_api/models.py
@@ -6,7 +6,7 @@ class Department(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     title = models.CharField(max_length=100)
     abbreviation = models.CharField(max_length=10)
-    imageurl = models.CharField(max_length=100, default="")
+    image = models.ImageField(upload_to="thumbnail", blank=True)
 
     def __str__(self):
         return self.title
--- studyportal/settings.py
+++ studyportal/settings.py
@@ -159,5 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 ELASTICSEARCH_DSL = {"default": {"hosts": "es:9200"}}
