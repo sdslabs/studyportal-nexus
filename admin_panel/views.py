f54849d8c27cb0ca2532ed624df69e325f0cd95a
mhk19 <gmahak1@gmail.com>
2021-03-11 02:12:22 +0530

    use if/else condition for upload of file object instead of another function

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -23,7 +23,6 @@ from rest_api.utils import (
     add_course,
     add_file,
     get_file_details_and_upload,
-    get_file_details_for_file_object,
     STRUCTURE,
 )
 from rest_api.serializers import CourseSerializer
@@ -56,8 +55,8 @@ class FileRequestViewSet(APIView):
             course = Course.objects.get(code=course_code)
             file_d = data["file"]
             name = data["name"]
-            file_details = get_file_details_for_file_object(
-                file_d, name, data["filetype"], course, False
+            file_details = get_file_details_and_upload(
+                file_d, name, data["filetype"], course, False, True
             )
             file = File(
                 title=get_title(file_d.name),
