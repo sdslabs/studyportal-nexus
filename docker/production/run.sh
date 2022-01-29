7e5a5afbd53be15b7fd6a8860dd493644f3ee37f
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2022-01-30 03:42:07 +0530

    feat(dockerfile): change dockerfile structure for new deployment architecture

---
+++ docker/production/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+while [[ "$(curl --insecure -s -o /dev/null -w '%{http_code}' http://study-es:9200/)" != "200" ]]
+do
+  echo "Waiting for Elasticsearch connection..."
+  sleep 2
+done
+
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+echo "Apply migration"
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Rebuild elasticsearch index"
+python manage.py search_index --rebuild -f
+gunicorn studyportal.wsgi:application --bind 0.0.0.0:8005 --workers 3
